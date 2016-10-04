9df81979556dcd23edb568e1350774b11cb60517
Koki Takahashi <hakatasiloving@gmail.com>
2016-10-05 01:15:17 +0900

    Fix specification of Redis URL

--- redis.js
+++ redis.js
@@ -4,6 +4,6 @@ const Promise = require('bluebird');
 Promise.promisifyAll(redis.RedisClient.prototype);
 Promise.promisifyAll(redis.Multi.prototype);
 
-const client = redis.createClient(process.env.REDIS_URL || '127.0.0.1:6379');
+const client = redis.createClient(process.env.REDIS_URL || 'redis://127.0.0.1:6379');
 
 module.exports = client;
