4095d966fbb985d21b99da043a10fbcb7e6836e9
Koki Takahashi <hakatasiloving@gmail.com>
2017-04-30 03:15:56 +0900

    FIXME: Ignore all cert errors

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// FIXME: Ignore all cert errors
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 const assignmentNotifier = require('./assignment-notifier');
 const davMonitor = require('./dav-monitor');
 const redis = require('./redis');
